9f8a0e967ffa7441fb2b48f9e861fd6aea6c921b
FernandoTrindade-0 <fernando.trindade1301@gmail.com>
2021-03-11 23:21:43 -0300

    filtros e botão limpar

--- lattes/filters.py
+++ lattes/filters.py
@@ -2,9 +2,12 @@ import django_filters
 from .models import Producao
 
 class ProducaoFilter(django_filters.FilterSet):
-
     class Meta:
         model = Producao
         fields = {
-            'ano': ['gte', 'lte']
+            'ano': ['gte', 'lte'],
+            'tipo': ['contains'],
+            'tipoAgrupador': ['contains'],
+            'informadoPor': ['contains'],
+            'titulo': ['contains']
         } 
