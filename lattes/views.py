08e3e65e3515b7a86b774e40d6930fe3ce145edf
FernandoTrindade-0 <fernando.trindade1301@gmail.com>
2021-03-18 23:57:12 -0300

    -graficos produção

--- lattes/views.py
+++ lattes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Producao
 from .filters import ProducaoFilter
-from django.db.models import Count
+from django.db.models import Count, Q
 
 # Create your views here.
 def index(request):
@@ -37,6 +37,27 @@ def perfilProducao(request):
 
     query = producoes_filtradas.qs.values('ano').annotate(total=Count('id')).order_by('ano')
     context['anos'] = [dic['ano'] for dic in query]
-    context['total'] = [dic['total'] for dic in query]
+    context['totalAno'] = [dic['total'] for dic in query]
+
+    query = producoes_filtradas.qs.values('tipo').annotate(total=Count('id')).order_by('tipo').filter(tipoAgrupador='Produção bibliográfica')
+    context['tiposB'] = [dic['tipo'] for dic in query]
+    context['totalTipoB'] = [dic['total'] for dic in query]
+
+    query = producoes_filtradas.qs.values('tipo').annotate(total=Count('id')).order_by('tipo').filter(tipoAgrupador='Produção técnica')
+    context['tiposT'] = [dic['tipo'] for dic in query]
+    context['totalTipoT'] = [dic['total'] for dic in query]
+
+    query = producoes_filtradas.qs.values('tipo').annotate(total=Count('id')).order_by('tipo').filter(tipoAgrupador='Orientação em andamento')
+    context['tiposO'] = [dic['tipo'] for dic in query]
+    context['totalTipoO'] = [dic['total'] for dic in query]
+
+    query = producoes_filtradas.qs.values('tipo').annotate(total=Count('id')).order_by('tipo').filter(tipoAgrupador='Produção artística/cultural')
+    context['tiposA'] = [dic['tipo'] for dic in query]
+    context['totalTipoA'] = [dic['total'] for dic in query]
+
+    query = producoes_filtradas.qs.values('tipo').annotate(total=Count('id')).order_by('tipo').filter(Q(tipoAgrupador='Banca') | Q(tipoAgrupador='Evento') | Q(tipoAgrupador='Outro tipo de produção'))
+    context['tiposE'] = [dic['tipo'] for dic in query]
+    context['totalTipoE'] = [dic['total'] for dic in query]
+
     context["producoes_filtradas"] = producoes_filtradas
     return render(request, "lattes/perfilProducao.html", context)
